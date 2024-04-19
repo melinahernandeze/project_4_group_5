ab4a8faea7a5a9689bfad0f91a8f6f102da88aa3
melinahernandeze <147454229+melinahernandeze@users.noreply.github.com>
2024-04-18 22:41:34 -0400

    2021_files

---
+++ HomeownershipData/2021 Homeownership.py
@@ -0,0 +1,206 @@
+homeownership,income,age,total,year
+owner,Less than $10000,Under 25 years old,42,2021
+owner,Less than $10000,25 to 29 years old,50,2021
+owner,Less than $10000,30 to 34 years old,109,2021
+owner,Less than $10000,35 to 44 years old,353,2021
+owner,Less than $10000,45 to 54 years old,527,2021
+owner,Less than $10000,55 to 64 years old,1091,2021
+owner,Less than $10000,65 to 74 years old,1155,2021
+owner,Less than $10000,75 years old and over,1162,2021
+owner,$10000 to $19999,Under 25 years old,0,2021
+owner,$10000 to $19999,25 to 29 years old,31,2021
+owner,$10000 to $19999,30 to 34 years old,61,2021
+owner,$10000 to $19999,35 to 44 years old,274,2021
+owner,$10000 to $19999,45 to 54 years old,369,2021
+owner,$10000 to $19999,55 to 64 years old,883,2021
+owner,$10000 to $19999,65 to 74 years old,1361,2021
+owner,$10000 to $19999,75 years old and over,1637,2021
+owner,$20000 to $29999,Under 25 years old,59,2021
+owner,$20000 to $29999,25 to 29 years old,107,2021
+owner,$20000 to $29999,30 to 34 years old,156,2021
+owner,$20000 to $29999,35 to 44 years old,369,2021
+owner,$20000 to $29999,45 to 54 years old,489,2021
+owner,$20000 to $29999,55 to 64 years old,1056,2021
+owner,$20000 to $29999,65 to 74 years old,1666,2021
+owner,$20000 to $29999,75 years old and over,1737,2021
+owner,$30000 to $39999,Under 25 years old,68,2021
+owner,$30000 to $39999,25 to 29 years old,184,2021
+owner,$30000 to $39999,30 to 34 years old,120,2021
+owner,$30000 to $39999,35 to 44 years old,502,2021
+owner,$30000 to $39999,45 to 54 years old,654,2021
+owner,$30000 to $39999,55 to 64 years old,1250,2021
+owner,$30000 to $39999,65 to 74 years old,1413,2021
+owner,$30000 to $39999,75 years old and over,1342,2021
+owner,$40000 to $49999,Under 25 years old,79,2021
+owner,$40000 to $49999,25 to 29 years old,220,2021
+owner,$40000 to $49999,30 to 34 years old,306,2021
+owner,$40000 to $49999,35 to 44 years old,601,2021
+owner,$40000 to $49999,45 to 54 years old,872,2021
+owner,$40000 to $49999,55 to 64 years old,1093,2021
+owner,$40000 to $49999,65 to 74 years old,1424,2021
+owner,$40000 to $49999,75 years old and over,1180,2021
+owner,$50000 to $59999,Under 25 years old,75,2021
+owner,$50000 to $59999,25 to 29 years old,208,2021
+owner,$50000 to $59999,30 to 34 years old,259,2021
+owner,$50000 to $59999,35 to 44 years old,604,2021
+owner,$50000 to $59999,45 to 54 years old,767,2021
+owner,$50000 to $59999,55 to 64 years old,1226,2021
+owner,$50000 to $59999,65 to 74 years old,1293,2021
+owner,$50000 to $59999,75 years old and over,864,2021
+owner,$60000 to $79999,Under 25 years old,110,2021
+owner,$60000 to $79999,25 to 29 years old,469,2021
+owner,$60000 to $79999,30 to 34 years old,647,2021
+owner,$60000 to $79999,35 to 44 years old,1643,2021
+owner,$60000 to $79999,45 to 54 years old,1677,2021
+owner,$60000 to $79999,55 to 64 years old,2470,2021
+owner,$60000 to $79999,65 to 74 years old,2074,2021
+owner,$60000 to $79999,75 years old and over,1314,2021
+owner,$80000 to $99999,Under 25 years old,73,2021
+owner,$80000 to $99999,25 to 29 years old,283,2021
+owner,$80000 to $99999,30 to 34 years old,642,2021
+owner,$80000 to $99999,35 to 44 years old,1593,2021
+owner,$80000 to $99999,45 to 54 years old,1597,2021
+owner,$80000 to $99999,55 to 64 years old,2082,2021
+owner,$80000 to $99999,65 to 74 years old,1459,2021
+owner,$80000 to $99999,75 years old and over,835,2021
+owner,$100000 to $119999,Under 25 years old,58,2021
+owner,$100000 to $119999,25 to 29 years old,310,2021
+owner,$100000 to $119999,30 to 34 years old,744,2021
+owner,$100000 to $119999,35 to 44 years old,1571,2021
+owner,$100000 to $119999,45 to 54 years old,1601,2021
+owner,$100000 to $119999,55 to 64 years old,1718,2021
+owner,$100000 to $119999,65 to 74 years old,1043,2021
+owner,$100000 to $119999,75 years old and over,499,2021
+owner,$120000 to $139999,Under 25 years old,186,2021
+owner,$120000 to $139999,25 to 29 years old,517,2021
+owner,$120000 to $139999,30 to 34 years old,1198,2021
+owner,$120000 to $139999,35 to 44 years old,1330,2021
+owner,$120000 to $139999,45 to 54 years old,1373,2021
+owner,$120000 to $139999,55 to 64 years old,704,2021
+owner,$120000 to $139999,65 to 74 years old,303,2021
+owner,$120000 to $139999,75 years old and over,,2021
+owner,$140000 to $159999,Under 25 years old,0,2021
+owner,$140000 to $159999,25 to 29 years old,184,2021
+owner,$140000 to $159999,30 to 34 years old,431,2021
+owner,$140000 to $159999,35 to 44 years old,1124,2021
+owner,$140000 to $159999,45 to 54 years old,1082,2021
+owner,$140000 to $159999,55 to 64 years old,1094,2021
+owner,$140000 to $159999,65 to 74 years old,547,2021
+owner,$140000 to $159999,75 years old and over,178,2021
+owner,$160000 to $179999,Under 25 years old,0,2021
+owner,$160000 to $179999,25 to 29 years old,104,2021
+owner,$160000 to $179999,30 to 34 years old,232,2021
+owner,$160000 to $179999,35 to 44 years old,734,2021
+owner,$160000 to $179999,45 to 54 years old,282,2021
+renter,Less than $10000,Under 25 years old,585,2021
+renter,Less than $10000,25 to 29 years old,499,2021
+renter,Less than $10000,30 to 34 years old,455,2021
+renter,Less than $10000,35 to 44 years old,759,2021
+renter,Less than $10000,45 to 54 years old,791,2021
+renter,Less than $10000,55 to 64 years old,1241,2021
+renter,Less than $10000,65 to 74 years old,828,2021
+renter,Less than $10000,75 years old and over,730,2021
+renter,$10000 to $19999,Under 25 years old,338,2021
+renter,$10000 to $19999,25 to 29 years old,395,2021
+renter,$10000 to $19999,30 to 34 years old,427,2021
+renter,$10000 to $19999,35 to 44 years old,782,2021
+renter,$10000 to $19999,45 to 54 years old,670,2021
+renter,$10000 to $19999,55 to 64 years old,1050,2021
+renter,$10000 to $19999,65 to 74 years old,1093,2021
+renter,$10000 to $19999,75 years old and over,909,2021
+renter,$20000 to $29999,Under 25 years old,562,2021
+renter,$20000 to $29999,25 to 29 years old,486,2021
+renter,$20000 to $29999,30 to 34 years old,571,2021
+renter,$20000 to $29999,35 to 44 years old,946,2021
+renter,$20000 to $29999,45 to 54 years old,744,2021
+renter,$20000 to $29999,55 to 64 years old,815,2021
+renter,$20000 to $29999,65 to 74 years old,595,2021
+renter,$20000 to $29999,75 years old and over,591,2021
+renter,$30000 to $39999,Under 25 years old,399,2021
+renter,$30000 to $39999,25 to 29 years old,662,2021
+renter,$30000 to $39999,30 to 34 years old,643,2021
+renter,$30000 to $39999,35 to 44 years old,1018,2021
+renter,$30000 to $39999,45 to 54 years old,830,2021
+renter,$30000 to $39999,55 to 64 years old,563,2021
+renter,$30000 to $39999,65 to 74 years old,538,2021
+renter,$30000 to $39999,75 years old and over,331,2021
+renter,$40000 to $49999,Under 25 years old,404,2021
+renter,$40000 to $49999,25 to 29 years old,602,2021
+renter,$40000 to $49999,30 to 34 years old,593,2021
+renter,$40000 to $49999,35 to 44 years old,965,2021
+renter,$40000 to $49999,45 to 54 years old,708,2021
+renter,$40000 to $49999,55 to 64 years old,640,2021
+renter,$40000 to $49999,65 to 74 years old,328,2021
+renter,$40000 to $49999,75 years old and over,234,2021
+renter,$50000 to $59999,Under 25 years old,360,2021
+renter,$50000 to $59999,25 to 29 years old,501,2021
+renter,$50000 to $59999,30 to 34 years old,502,2021
+renter,$50000 to $59999,35 to 44 years old,816,2021
+renter,$50000 to $59999,45 to 54 years old,577,2021
+renter,$50000 to $59999,55 to 64 years old,471,2021
+renter,$50000 to $59999,65 to 74 years old,215,2021
+renter,$50000 to $59999,75 years old and over,138,2021
+renter,$60000 to $79999,Under 25 years old,471,2021
+renter,$60000 to $79999,25 to 29 years old,864,2021
+renter,$60000 to $79999,30 to 34 years old,829,2021
+renter,$60000 to $79999,35 to 44 years old,1411,2021
+renter,$60000 to $79999,45 to 54 years old,967,2021
+renter,$60000 to $79999,55 to 64 years old,688,2021
+renter,$60000 to $79999,65 to 74 years old,330,2021
+renter,$60000 to $79999,75 years old and over,181,2021
+renter,$80000 to $99999,Under 25 years old,216,2021
+renter,$80000 to $99999,25 to 29 years old,522,2021
+renter,$80000 to $99999,30 to 34 years old,660,2021
+renter,$80000 to $99999,35 to 44 years old,799,2021
+renter,$80000 to $99999,45 to 54 years old,621,2021
+renter,$80000 to $99999,55 to 64 years old,396,2021
+renter,$80000 to $99999,65 to 74 years old,153,2021
+renter,$80000 to $99999,75 years old and over,73,2021
+renter,$100000 to $119999,Under 25 years old,19,2021
+renter,$100000 to $119999,25 to 29 years old,274,2021
+renter,$100000 to $119999,30 to 34 years old,421,2021
+renter,$100000 to $119999,35 to 44 years old,524,2021
+renter,$100000 to $119999,45 to 54 years old,383,2021
+renter,$100000 to $119999,55 to 64 years old,276,2021
+renter,$100000 to $119999,65 to 74 years old,93,2021
+renter,$100000 to $119999,75 years old and over,44,2021
+renter,$120000 to $139999,Under 25 years old,78,2021
+renter,$120000 to $139999,25 to 29 years old,236,2021
+renter,$120000 to $139999,30 to 34 years old,256,2021
+renter,$120000 to $139999,35 to 44 years old,439,2021
+renter,$120000 to $139999,45 to 54 years old,299,2021
+renter,$120000 to $139999,55 to 64 years old,154,2021
+renter,$120000 to $139999,65 to 74 years old,57,2021
+renter,$120000 to $139999,75 years old and over,40,2021
+renter,$140000 to $159999,Under 25 years old,903,2021
+renter,$140000 to $159999,25 to 29 years old,53,2021
+renter,$140000 to $159999,30 to 34 years old,127,2021
+renter,$140000 to $159999,35 to 44 years old,143,2021
+renter,$140000 to $159999,45 to 54 years old,233,2021
+renter,$140000 to $159999,55 to 64 years old,171,2021
+renter,$140000 to $159999,65 to 74 years old,93,2021
+renter,$140000 to $159999,75 years old and over,65,2021
+renter,$160000 to $179999,Under 25 years old,28,2021
+renter,$160000 to $179999,25 to 29 years old,89,2021
+renter,$160000 to $179999,30 to 34 years old,126,2021
+renter,$160000 to $179999,35 to 44 years old,128,2021
+renter,$160000 to $179999,45 to 54 years old,96,2021
+renter,$160000 to $179999,55 to 64 years old,45,2021
+renter,$160000 to $179999,65 to 74 years old,56,2021
+renter,$160000 to $179999,75 years old and over,43,2021
+renter,$180000 to $199999,Under 25 years old,396,2021
+renter,$180000 to $199999,25 to 29 years old,0,2021
+renter,$180000 to $199999,30 to 34 years old,61,2021
+renter,$180000 to $199999,35 to 44 years old,99,2021
+renter,$180000 to $199999,45 to 54 years old,102,2021
+renter,$180000 to $199999,55 to 64 years old,70,2021
+renter,$180000 to $199999,65 to 74 years old,43,2021
+renter,$180000 to $199999,75 years old and over,63,2021
+renter,$200000 or more,Under 25 years old,56,2021
+renter,$200000 or more,25 to 29 years old,188,2021
+renter,$200000 or more,30 to 34 years old,264,2021
+renter,$200000 or more,35 to 44 years old,337,2021
+renter,$200000 or more,45 to 54 years old,330,2021
+renter,$200000 or more,55 to 64 years old,157,2021
+renter,$200000 or more,65 to 74 years old,37,2021
+renter,$200000 or more,75 years old and over,0,2021
