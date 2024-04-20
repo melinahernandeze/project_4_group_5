d4d0335825142c84095545569bd0b5720483bc02
melinahernandeze <147454229+melinahernandeze@users.noreply.github.com>
2024-04-20 09:03:38 -0400

    datasets

---
+++ HomeownershipData/2013_Data.py
@@ -0,0 +1,161 @@
+homeownership,income,age,total,year
+owner,Less than $10000,Under 25 years old,91,2013
+owner,Less than $10000,25 to 29 years old,155,2013
+owner,Less than $10000,30 to 34 years old,232,2013
+owner,Less than $10000,35 to 44 years old,367,2013
+owner,Less than $10000,45 to 54 years old,717,2013
+owner,Less than $10000,55 to 64 years old,1005,2013
+owner,Less than $10000,65 to 74 years old,866,2013
+owner,Less than $10000,75 years old and over,1192,2013
+owner,$10000 to $19999,Under 25 years old,56,2013
+owner,$10000 to $19999,25 to 29 years old,126,2013
+owner,$10000 to $19999,30 to 34 years old,179,2013
+owner,$10000 to $19999,35 to 44 years old,383,2013
+owner,$10000 to $19999,45 to 54 years old,588,2013
+owner,$10000 to $19999,55 to 64 years old,1087,2013
+owner,$10000 to $19999,65 to 74 years old,1404,2013
+owner,$10000 to $19999,75 years old and over,2027,2013
+owner,$20000 to $29999,Under 25 years old,110,2013
+owner,$20000 to $29999,25 to 29 years old,200,2013
+owner,$20000 to $29999,30 to 34 years old,344,2013
+owner,$20000 to $29999,35 to 44 years old,686,2013
+owner,$20000 to $29999,45 to 54 years old,1116,2013
+owner,$20000 to $29999,55 to 64 years old,1524,2013
+owner,$20000 to $29999,65 to 74 years old,1855,2013
+owner,$20000 to $29999,75 years old and over,1948,2013
+owner,$30000 to $39999,Under 25 years old,139,2013
+owner,$30000 to $39999,25 to 29 years old,243,2013
+owner,$30000 to $39999,30 to 34 years old,416,2013
+owner,$30000 to $39999,35 to 44 years old,786,2013
+owner,$30000 to $39999,45 to 54 years old,940,2013
+owner,$30000 to $39999,55 to 64 years old,1466,2013
+owner,$30000 to $39999,65 to 74 years old,1450,2013
+owner,$30000 to $39999,75 years old and over,1228,2013
+owner,$40000 to $49999,Under 25 years old,111,2013
+owner,$40000 to $49999,25 to 29 years old,346,2013
+owner,$40000 to $49999,30 to 34 years old,353,2013
+owner,$40000 to $49999,35 to 44 years old,822,2013
+owner,$40000 to $49999,45 to 54 years old,1217,2013
+owner,$40000 to $49999,55 to 64 years old,1371,2013
+owner,$40000 to $49999,65 to 74 years old,1195,2013
+owner,$40000 to $49999,75 years old and over,891,2013
+owner,$50000 to $59999,Under 25 years old,68,2013
+owner,$50000 to $59999,25 to 29 years old,339,2013
+owner,$50000 to $59999,30 to 34 years old,466,2013
+owner,$50000 to $59999,35 to 44 years old,915,2013
+owner,$50000 to $59999,45 to 54 years old,1185,2013
+owner,$50000 to $59999,55 to 64 years old,1268,2013
+owner,$50000 to $59999,65 to 74 years old,943,2013
+owner,$50000 to $59999,75 years old and over,622,2013
+owner,$60000 to $79999,Under 25 years old,111,2013
+owner,$60000 to $79999,25 to 29 years old,495,2013
+owner,$60000 to $79999,30 to 34 years old,878,2013
+owner,$60000 to $79999,35 to 44 years old,1927,2013
+owner,$60000 to $79999,45 to 54 years old,2331,2013
+owner,$60000 to $79999,55 to 64 years old,2217,2013
+owner,$60000 to $79999,65 to 74 years old,1447,2013
+owner,$60000 to $79999,75 years old and over,829,2013
+owner,$80000 to $99999,Under 25 years old,25,2013
+owner,$80000 to $99999,25 to 29 years old,371,2013
+owner,$80000 to $99999,30 to 34 years old,766,2013
+owner,$80000 to $99999,35 to 44 years old,1670,2013
+owner,$80000 to $99999,45 to 54 years old,2007,2013
+owner,$80000 to $99999,55 to 64 years old,1714,2013
+owner,$80000 to $99999,65 to 74 years old,925,2013
+owner,$80000 to $99999,75 years old and over,425,2013
+owner,$100000 to $119999,Under 25 years old,13,2013
+owner,$100000 to $119999,25 to 29 years old,265,2013
+owner,$100000 to $119999,30 to 34 years old,556,2013
+owner,$100000 to $119999,35 to 44 years old,1315,2013
+owner,$100000 to $119999,45 to 54 years old,1645,2013
+owner,$100000 to $119999,55 to 64 years old,1420,2013
+owner,$100000 to $119999,65 to 74 years old,499,2013
+owner,$100000 to $119999,75 years old and over,227,2013
+owner,$120000 or more,Under 25 years old,46,2013
+owner,$120000 or more,25 to 29 years old,336,2013
+owner,$120000 or more,30 to 34 years old,1083,2013
+owner,$120000 or more,35 to 44 years old,3320,2013
+owner,$120000 or more,45 to 54 years old,4587,2013
+owner,$120000 or more,55 to 64 years old,3494,2013
+owner,$120000 or more,65 to 74 years old,1306,2013
+owner,$120000 or more,75 years old and over,361,20132
+renter,Less than $10000,Under 25 years old,1018,2013
+renter,Less than $10000,25 to 29 years old,705,2013
+renter,Less than $10000,30 to 34 years old,641,2013
+renter,Less than $10000,35 to 44 years old,956,2013
+renter,Less than $10000,45 to 54 years old,1145,2013
+renter,Less than $10000,55 to 64 years old,1018,2013
+renter,Less than $10000,65 to 74 years old,658,2013
+renter,Less than $10000,75 years old and over,643,2013
+renter,$10000 to $19999,Under 25 years old,752,2013
+renter,$10000 to $19999,25 to 29 years old,666,2013
+renter,$10000 to $19999,30 to 34 years old,589,2013
+renter,$10000 to $19999,35 to 44 years old,967,2013
+renter,$10000 to $19999,45 to 54 years old,926,2013
+renter,$10000 to $19999,55 to 64 years old,985,2013
+renter,$10000 to $19999,65 to 74 years old,704,2013
+renter,$10000 to $19999,75 years old and over,840,2013
+renter,$20000 to $29999,Under 25 years old,708,2013
+renter,$20000 to $29999,25 to 29 years old,792,2013
+renter,$20000 to $29999,30 to 34 years old,839,2013
+renter,$20000 to $29999,35 to 44 years old,1157,2013
+renter,$20000 to $29999,45 to 54 years old,984,2013
+renter,$20000 to $29999,55 to 64 years old,844,2013
+renter,$20000 to $29999,65 to 74 years old,440,2013
+renter,$20000 to $29999,75 years old and over,421,2013
+renter,$30000 to $39999,Under 25 years old,596,2013
+renter,$30000 to $39999,25 to 29 years old,751,2013
+renter,$30000 to $39999,30 to 34 years old,678,2013
+renter,$30000 to $39999,35 to 44 years old,958,2013
+renter,$30000 to $39999,45 to 54 years old,754,2013
+renter,$30000 to $39999,55 to 64 years old,565,2013
+renter,$30000 to $39999,65 to 74 years old,281,2013
+renter,$30000 to $39999,75 years old and over,222,2013
+renter,$40000 to $49999,Under 25 years old,424,2013
+renter,$40000 to $49999,25 to 29 years old,645,2013
+renter,$40000 to $49999,30 to 34 years old,554,2013
+renter,$40000 to $49999,35 to 44 years old,895,2013
+renter,$40000 to $49999,45 to 54 years old,701,2013
+renter,$40000 to $49999,55 to 64 years old,407,2013
+renter,$40000 to $49999,65 to 74 years old,171,2013
+renter,$40000 to $49999,75 years old and over,106,2013
+renter,$50000 to $59999,Under 25 years old,206,2013
+renter,$50000 to $59999,25 to 29 years old,470,2013
+renter,$50000 to $59999,30 to 34 years old,483,2013
+renter,$50000 to $59999,35 to 44 years old,634,2013
+renter,$50000 to $59999,45 to 54 years old,517,2013
+renter,$50000 to $59999,55 to 64 years old,279,2013
+renter,$50000 to $59999,65 to 74 years old,116,2013
+renter,$50000 to $59999,75 years old and over,71,2013
+renter,$60000 to $79999,Under 25 years old,369,2013
+renter,$60000 to $79999,25 to 29 years old,659,2013
+renter,$60000 to $79999,30 to 34 years old,709,2013
+renter,$60000 to $79999,35 to 44 years old,986,2013
+renter,$60000 to $79999,45 to 54 years old,710,2013
+renter,$60000 to $79999,55 to 64 years old,408,2013
+renter,$60000 to $79999,65 to 74 years old,125,2013
+renter,$60000 to $79999,75 years old and over,75,2013
+renter,$80000 to $99999,Under 25 years old,102,2013
+renter,$80000 to $99999,25 to 29 years old,382,2013
+renter,$80000 to $99999,30 to 34 years old,430,2013
+renter,$80000 to $99999,35 to 44 years old,511,2013
+renter,$80000 to $99999,45 to 54 years old,353,2013
+renter,$80000 to $99999,55 to 64 years old,220,2013
+renter,$80000 to $99999,65 to 74 years old,70,2013
+renter,$80000 to $99999,75 years old and over,36,2013
+renter,$100000 to $119999,Under 25 years old,62,2013
+renter,$100000 to $119999,25 to 29 years old,218,2013
+renter,$100000 to $119999,30 to 34 years old,218,2013
+renter,$100000 to $119999,35 to 44 years old,278,2013
+renter,$100000 to $119999,45 to 54 years old,235,2013
+renter,$100000 to $119999,55 to 64 years old,135,2013
+renter,$100000 to $119999,65 to 74 years old,35,2013
+renter,$100000 to $119999,75 years old and over,32,2013
+renter,$120000 or more,Under 25 years old,108,2013
+renter,$120000 or more,25 to 29 years old,276,2013
+renter,$120000 or more,30 to 34 years old,366,2013
+renter,$120000 or more,35 to 44 years old,506,2013
+renter,$120000 or more,45 to 54 years old,375,2013
+renter,$120000 or more,55 to 64 years old,230,2013
+renter,$120000 or more,65 to 74 years old,75,2013
+renter,$120000 or more,75 years old and over,26,2013
---
+++ HomeownershipData/2017_Data.py
@@ -0,0 +1,161 @@
+homeownership,income,age,total,year
+owner,Less than $10000,Under 25 years old,44,2017
+owner,Less than $10000,25 to 29 years old,61,2017
+owner,Less than $10000,30 to 34 years old,73,2017
+owner,Less than $10000,35 to 44 years old,336,2017
+owner,Less than $10000,45 to 54 years old,617,2017
+owner,Less than $10000,55 to 64 years old,997,2017
+owner,Less than $10000,65 to 74 years old,1050,2017
+owner,Less than $10000,75 years old and over,1134,2017
+owner,$10000 to $19999,Under 25 years old,47,2017
+owner,$10000 to $19999,25 to 29 years old,34,2017
+owner,$10000 to $19999,30 to 34 years old,107,2017
+owner,$10000 to $19999,35 to 44 years old,236,2017
+owner,$10000 to $19999,45 to 54 years old,448,2017
+owner,$10000 to $19999,55 to 64 years old,1051,2017
+owner,$10000 to $19999,65 to 74 years old,1430,2017
+owner,$10000 to $19999,75 years old and over,1596,2017
+owner,$20000 to $29999,Under 25 years old,57,2017
+owner,$20000 to $29999,25 to 29 years old,125,2017
+owner,$20000 to $29999,30 to 34 years old,188,2017
+owner,$20000 to $29999,35 to 44 years old,376,2017
+owner,$20000 to $29999,45 to 54 years old,754,2017
+owner,$20000 to $29999,55 to 64 years old,1212,2017
+owner,$20000 to $29999,65 to 74 years old,1612,2017
+owner,$20000 to $29999,75 years old and over,1669,2017
+owner,$30000 to $39999,Under 25 years old,0,2017
+owner,$30000 to $39999,25 to 29 years old,0,2017
+owner,$30000 to $39999,30 to 34 years old,0,2017
+owner,$30000 to $39999,35 to 44 years old,59,2017
+owner,$30000 to $39999,45 to 54 years old,143,2017
+owner,$30000 to $39999,55 to 64 years old,277,2017
+owner,$30000 to $39999,65 to 74 years old,617,2017
+owner,$30000 to $39999,75 years old and over,866,2017
+owner,$40000 to $49999,Under 25 years old,91,2017
+owner,$40000 to $49999,25 to 29 years old,206,2017
+owner,$40000 to $49999,30 to 34 years old,236,2017
+owner,$40000 to $49999,35 to 44 years old,677,2017
+owner,$40000 to $49999,45 to 54 years old,910,2017
+owner,$40000 to $49999,55 to 64 years old,1421,2017
+owner,$40000 to $49999,65 to 74 years old,1498,2017
+owner,$40000 to $49999,75 years old and over,942,2017
+owner,$50000 to $59999,Under 25 years old,78,2017
+owner,$50000 to $59999,25 to 29 years old,199,2017
+owner,$50000 to $59999,30 to 34 years old,255,2017
+owner,$50000 to $59999,35 to 44 years old,819,2017
+owner,$50000 to $59999,45 to 54 years old,1070,2017
+owner,$50000 to $59999,55 to 64 years old,1378,2017
+owner,$50000 to $59999,65 to 74 years old,1069,2017
+owner,$50000 to $59999,75 years old and over,720,2017
+owner,$60000 to $79999,Under 25 years old,79,2017
+owner,$60000 to $79999,25 to 29 years old,525,2017
+owner,$60000 to $79999,30 to 34 years old,714,2017
+owner,$60000 to $79999,35 to 44 years old,1597,2017
+owner,$60000 to $79999,45 to 54 years old,2173,2017
+owner,$60000 to $79999,55 to 64 years old,2458,2017
+owner,$60000 to $79999,65 to 74 years old,1854,2017
+owner,$60000 to $79999,75 years old and over,999,2017
+owner,$80000 to $99999,Under 25 years old,41,2017
+owner,$80000 to $99999,25 to 29 years old,320,2017
+owner,$80000 to $99999,30 to 34 years old,679,2017
+owner,$80000 to $99999,35 to 44 years old,1640,2017
+owner,$80000 to $99999,45 to 54 years old,1775,2017
+owner,$80000 to $99999,55 to 64 years old,2081,2017
+owner,$80000 to $99999,65 to 74 years old,1261,2017
+owner,$80000 to $99999,75 years old and over,568,2017
+owner,$100000 to $119999,Under 25 years old,176,2017
+owner,$100000 to $119999,25 to 29 years old,246,2017
+owner,$100000 to $119999,30 to 34 years old,566,2017
+owner,$100000 to $119999,35 to 44 years old,1473,2017
+owner,$100000 to $119999,45 to 54 years old,1721,2017
+owner,$100000 to $119999,55 to 64 years old,1713,2017
+owner,$100000 to $119999,65 to 74 years old,896,2017
+owner,$100000 to $119999,75 years old and over,251,2017
+owner,$120000 or more,Under 25 years old,54,2017
+owner,$120000 or more,25 to 29 years old,452,2017
+owner,$120000 or more,30 to 34 years old,1350,2017
+owner,$120000 or more,35 to 44 years old,4279,2017
+owner,$120000 or more,45 to 54 years old,5462,2017
+owner,$120000 or more,55 to 64 years old,4779,2017
+owner,$120000 or more,65 to 74 years old,1951,2017
+owner,$120000 or more,75 years old and over,554,2017
+renter,Less than $10000,Under 25 years old,707,2017
+renter,Less than $10000,25 to 29 years old,559,2017
+renter,Less than $10000,30 to 34 years old,508,2017
+renter,Less than $10000,35 to 44 years old,865,2017
+renter,Less than $10000,45 to 54 years old,868,2017
+renter,Less than $10000,55 to 64 years old,1169,2017
+renter,Less than $10000,65 to 74 years old,800,2017
+renter,Less than $10000,75 years old and over,715,2017
+renter,$10000 to $19999,Under 25 years old,490,2017
+renter,$10000 to $19999,25 to 29 years old,499,2017
+renter,$10000 to $19999,30 to 34 years old,451,2017
+renter,$10000 to $19999,35 to 44 years old,901,2017
+renter,$10000 to $19999,45 to 54 years old,910,2017
+renter,$10000 to $19999,55 to 64 years old,1094,2017
+renter,$10000 to $19999,65 to 74 years old,839,2017
+renter,$10000 to $19999,75 years old and over,1010,2017
+renter,$20000 to $29999,Under 25 years old,717,2017
+renter,$20000 to $29999,25 to 29 years old,642,2017
+renter,$20000 to $29999,30 to 34 years old,1051,2017
+renter,$20000 to $29999,35 to 44 years old,875,2017
+renter,$20000 to $29999,45 to 54 years old,893,2017
+renter,$20000 to $29999,55 to 64 years old,583,2017
+renter,$20000 to $29999,65 to 74 years old,494,2017
+renter,$20000 to $29999,75 years old and over,,2017
+renter,$30000 to $39999,Under 25 years old,498,2017
+renter,$30000 to $39999,25 to 29 years old,667,2017
+renter,$30000 to $39999,30 to 34 years old,506,2017
+renter,$30000 to $39999,35 to 44 years old,1147,2017
+renter,$30000 to $39999,45 to 54 years old,772,2017
+renter,$30000 to $39999,55 to 64 years old,703,2017
+renter,$30000 to $39999,65 to 74 years old,370,2017
+renter,$30000 to $39999,75 years old and over,308,2017
+renter,$40000 to $49999,Under 25 years old,426,2017
+renter,$40000 to $49999,25 to 29 years old,581,2017
+renter,$40000 to $49999,30 to 34 years old,557,2017
+renter,$40000 to $49999,35 to 44 years old,893,2017
+renter,$40000 to $49999,45 to 54 years old,774,2017
+renter,$40000 to $49999,55 to 64 years old,570,2017
+renter,$40000 to $49999,65 to 74 years old,326,2017
+renter,$40000 to $49999,75 years old and over,158,2017
+renter,$50000 to $59999,Under 25 years old,264,2017
+renter,$50000 to $59999,25 to 29 years old,559,2017
+renter,$50000 to $59999,30 to 34 years old,514,2017
+renter,$50000 to $59999,35 to 44 years old,783,2017
+renter,$50000 to $59999,45 to 54 years old,629,2017
+renter,$50000 to $59999,55 to 64 years old,356,2017
+renter,$50000 to $59999,65 to 74 years old,203,2017
+renter,$50000 to $59999,75 years old and over,110,2017
+renter,$60000 to $79999,Under 25 years old,375,2017
+renter,$60000 to $79999,25 to 29 years old,738,2017
+renter,$60000 to $79999,30 to 34 years old,764,2017
+renter,$60000 to $79999,35 to 44 years old,1119,2017
+renter,$60000 to $79999,45 to 54 years old,954,2017
+renter,$60000 to $79999,55 to 64 years old,595,2017
+renter,$60000 to $79999,65 to 74 years old,206,2017
+renter,$60000 to $79999,75 years old and over,104,2017
+renter,$80000 to $99999,Under 25 years old,147,2017
+renter,$80000 to $99999,25 to 29 years old,478,2017
+renter,$80000 to $99999,30 to 34 years old,522,2017
+renter,$80000 to $99999,35 to 44 years old,691,2017
+renter,$80000 to $99999,45 to 54 years old,596,2017
+renter,$80000 to $99999,55 to 64 years old,359,2017
+renter,$80000 to $99999,65 to 74 years old,120,2017
+renter,$80000 to $99999,75 years old and over,48,2017
+renter,$100000 to $119999,Under 25 years old,100,2017
+renter,$100000 to $119999,25 to 29 years old,314,2017
+renter,$100000 to $119999,30 to 34 years old,281,2017
+renter,$100000 to $119999,35 to 44 years old,460,2017
+renter,$100000 to $119999,45 to 54 years old,370,2017
+renter,$100000 to $119999,55 to 64 years old,206,2017
+renter,$100000 to $119999,65 to 74 years old,76,2017
+renter,$100000 to $119999,75 years old and over,36,2017
+renter,$120000 or more,Under 25 years old,161,2017
+renter,$120000 or more,25 to 29 years old,564,2017
+renter,$120000 or more,30 to 34 years old,585,2017
+renter,$120000 or more,35 to 44 years old,859,2017
+renter,$120000 or more,45 to 54 years old,749,2017
+renter,$120000 or more,55 to 64 years old,393,2017
+renter,$120000 or more,65 to 74 years old,137,2017
+renter,$120000 or more,75 years old and over,41,2017
